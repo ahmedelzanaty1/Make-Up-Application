a4eee370f7615870a16e653509f902cc3a25617f
Hp <ahmedelzanaty001@gmail.com>
2025-05-11 18:46:33 +0300

    init commit

---
+++ app/src/main/java/com/example/makeupapplication/common/Destinations.kt
@@ -0,0 +1,12 @@
+package com.example.makeupapplication.common
+
+object Destinations {
+    const val SPLASH = "splash"
+    const val INTRO = "intro"
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val HOME = "home"
+
+
+
+}
